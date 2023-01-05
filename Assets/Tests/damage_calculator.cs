6b3a5980122f022d8d84183d5867e87ef8a4c19d
UmutCanT <uctekcambusiness@gmail.com>
2023-01-05 15:22:56 +0300

    Scenario 2

--- Assets/Tests/damage_calculator.cs
+++ Assets/Tests/damage_calculator.cs
@@ -9,6 +9,20 @@ public class damage_calculator
     [Test]
     public void Sets_damage_to_half_with_50_percent_mitigation()
     {
+        // ACT
         int finalDamage = DamageCalculator.CalculateDamage(10, 0.5f);
+
+        //ASSERT
+        Assert.AreEqual(5, finalDamage);
+    }
+
+    [Test]
+    public void Calculates_2_damage_from_10_with_80_percent_mitigation()
+    {
+        // ACT
+        int finalDamage = DamageCalculator.CalculateDamage(10, 0.8f);
+
+        //ASSERT
+        Assert.AreEqual(2, finalDamage);
     }
 }
